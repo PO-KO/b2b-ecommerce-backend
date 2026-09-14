c31667a4d104915aa05777426c73fc3280797f6a
PO-KO <100231639+PO-KO@users.noreply.github.com>
2026-09-14 10:26:04 +0100

    Update db.config.ts

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -11,7 +11,7 @@ export const pgConfig: TypeOrmModuleOptions = {
   // username: 'postgres',
   // password: 'postgres',
   // database: 'b2b_ecommerce',
-  url: 'postgresql://neondb_owner:npg_sUHaRTPc5pk3@ep-divine-king-ayhrcxkd-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  url: 'ENV_FILE',
   autoLoadEntities: true,
   // Production synchronize = false
   synchronize: true,
